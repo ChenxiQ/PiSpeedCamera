e267e8c227d75f83282c7ba2baea59c052fe7119
ChenxiQ <qianchenxi20081114@163.com>
2021-12-03 17:51:04 -0500

    OCR finished

--- Script/main.py
+++ Script/main.py
@@ -1,6 +1,7 @@
 from MeasureSpeed import measureSpeed
 from CaptureLicense import captureLicense
 from LicenseDetect import licenseDetect
+from LicenseOCR import licenseOCR
 
 SPEED_LIMIT = 0  # speed limit 20 mile/h
 
@@ -8,3 +9,4 @@ detectedSpeed = measureSpeed()
 if detectedSpeed > SPEED_LIMIT:
     captureTime, originalImagePath = captureLicense()
     captureTime, croppedImagePath = licenseDetect(captureTime, originalImagePath)
+    licenseOCR(captureTime, croppedImagePath)
