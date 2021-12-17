e4251d83b7955b02a743a700ef9c1c5fd401ad84
ChenxiQ <cq53@cornell.edu>
2021-12-16 21:29:50 -0500

    syntax improvement

--- PiWeb/Sensors_Database/WebServer/webServer.py
+++ PiWeb/Sensors_Database/WebServer/webServer.py
@@ -1,7 +1,8 @@
 '''
-	webServer.py
- 	Created by Kuan Lu   
-	Build the server with Flask.
+  webServer.py
+  
+  Created by Kuan Lu   
+  Build the server with Flask.
 '''
 
 from flask import Flask, render_template, request
@@ -11,29 +12,29 @@ import sqlite3
 
 # Retrieve data from database
 def getData():
-	conn=sqlite3.connect('/home/pi/PiWeb/Sensors_Database/test.db')
-	curs=conn.cursor()
-	time = []
-	licensePlate = []
-	speed = []
-	for row in curs.execute("SELECT * FROM DHT_data ORDER BY timestamp DESC LIMIT 5"):
-		time.append(str(row[0]))
-		licensePlate.append(row[1])
-		speed.append(row[2])
-	conn.close()
-	return time, licensePlate, speed 
+  conn=sqlite3.connect('/home/pi/PiWeb/Sensors_Database/test.db')
+  curs=conn.cursor()
+  time = []
+  licensePlate = []
+  speed = []
+  for row in curs.execute("SELECT * FROM DHT_data ORDER BY timestamp DESC LIMIT 5"):
+    time.append(str(row[0]))
+    licensePlate.append(row[1])
+    speed.append(row[2])
+  conn.close()
+  return time, licensePlate, speed 
 
 # Upload the data to html 
 @app.route("/")
 def index():
-	
-	time, licensePlate, speed = getData()
-	templateData = {
-	  'time'	: time,
-      'license'	: licensePlate,
-      'speed'		: speed
-	}
-	return render_template('index.html', **templateData)
+  
+  time, licensePlate, speed = getData()
+  templateData = {
+    'time'    : time,
+    'license' : licensePlate,
+    'speed'   : speed
+  }
+  return render_template('index.html', **templateData)
 
 
 if __name__ == "__main__":
--- PiWeb/Sensors_Database/database.py
+++ PiWeb/Sensors_Database/database.py
@@ -1,13 +1,13 @@
 '''
-	database.py
- 	Created by Kuan Lu   
-	Creat database with SQLite.
+    database.py
+    Created by Kuan Lu
+    Creat database with SQLite.
 '''
 
 import sqlite3 as lite
 import sys
 con = lite.connect('test.db')
 with con: 
-    cur = con.cursor() 
+    cur = con.cursor()
     cur.execute("DROP TABLE IF EXISTS data")
     cur.execute("CREATE TABLE DHT_data(timestamp DATETIME, license TEXT, speed NUMERIC)")
